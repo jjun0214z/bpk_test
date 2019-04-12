7cb7ed4630b84162c0d1a161c8b2450c291f48d3
jjun0214z <jjun0214z@gmail.com>
2019-04-12 14:31:51 +0900

    Updates

---
+++ precache-manifest.dd88472e9866ef5d4067496dc0cc3cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c75031a518f0132fd9cbac749fec338",
+    "url": "/bpk/static/media/img_card.3c75031a.png"
+  },
+  {
+    "revision": "3ef1a38a17e106316325",
+    "url": "/bpk/static/js/runtime~main.8f79c00d.js"
+  },
+  {
+    "revision": "f083151966eef5dc1227",
+    "url": "/bpk/static/js/main.a722e902.chunk.js"
+  },
+  {
+    "revision": "2e134b0a72e5cb5f0979",
+    "url": "/bpk/static/js/2.11a53da7.chunk.js"
+  },
+  {
+    "revision": "f083151966eef5dc1227",
+    "url": "/bpk/static/css/main.a276a20d.chunk.css"
+  },
+  {
+    "revision": "209030c9e39b26d61a0bbfea093cbd41",
+    "url": "/bpk/index.html"
+  }
+];
