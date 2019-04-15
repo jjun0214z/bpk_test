de8de262ce9215fbb4aa41eed53b35240e021e58
jjun0214z <jjun0214z@gmail.com>
2019-04-15 15:57:40 +0900

    .

--- frontend/src/components/Comment/container.js
+++ frontend/src/components/Comment/container.js
@@ -15,16 +15,19 @@ class Container extends Component {
                     handleInputChanage = {this._handleInputChanage}
                     handleMaxlength = {this._handleMaxlength}
                     textState={"default"}
+                    title="입력영역"
                 />
                 <Comment 
                     {...this.state}
                     handleInputChanage = {this._handleInputChanage}
                     textState={"disabled"}
+                    title="입력영역"
                 />
                 <Comment 
                     {...this.state}
                     handleInputChanage = {this._handleInputChanage}
                     textState={"readOnly"}
+                    title="입력영역"
                 />
             </div>
         );
